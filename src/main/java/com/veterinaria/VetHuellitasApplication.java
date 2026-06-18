d11cd49dce4f80b9860a63251c58ddd37b9b9e51
yutu202525-rgb <yutu202525@gmail.com>
2026-06-18 16:50:55 -0500

    Initial commit

---
+++ src/main/java/com/veterinaria/VetHuellitasApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetHuellitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetHuellitasApplication.class, args);
+	}
+
+}
